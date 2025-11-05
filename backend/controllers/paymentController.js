16550fb8c7e797b9f43f36b4d69165d1c58b72cf
phuongtcn2911 <phuong.tcn2911@gmail.com>
2025-11-05 18:43:02 +0700

    update function on PaymentController

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -1,7 +1,11 @@
-export async function createPayment(req,res){
+export async function createPayment(req, res) {
 
 }
 
-export async function confirmPaymentSePay(req,res){
+export async function confirmPaymentSePay(req, res) {
+    console.log("📩 Webhook từ SePay:", req.body);
+
+    // Gửi phản hồi lại SePay
+    res.json({ success: true });
 
 }
--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -2,6 +2,7 @@ import express from "express";
 import * as paymentController from"../controllers/paymentController.js";
 
 const router =express.Router();
+
 router.post("/createPayment",paymentController.createPayment);
 router.post("/confirmPaymentSePay",paymentController.confirmPaymentSePay);
 
